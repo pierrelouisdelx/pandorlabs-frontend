79983ab4b6786507fa6d45aaac5ade4af7628c01
Pierre-Louis <pierrelouisdelcroix2001@gmail.com>
2025-11-05 00:15:26 +0100

    Update 404 page metadata and improve content structure
    - Changed the title and description for the 404 page to reflect the new branding as "PandorLabs".
    - Enhanced the open graph data for better SEO and social sharing.
    - Improved the layout and readability of the 404 error message and call-to-action link.
    - Ensured consistent styling and structure throughout the component.

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,60 +5,59 @@ import { Metadata } from 'next'
 import Link from 'next/link'
 
 export const metadata: Metadata = {
-    title: '404 | InfynixAI',
+  title: '404 | PandorLabs',
+  description:
+    "We couldn't find the page you're looking for. It might have been moved, renamed, or is temporarily unavailable. Let's help you get back on track!",
+  openGraph: {
+    ...helper.openGraphData,
+    title: '404 | PandorLabs',
     description:
-        'Enhance your service offerings and streamline processes with our intuitive AI SaaS template.',
-    openGraph: {
-        ...helper.openGraphData,
-        title: '404 | InfynixAI',
-        description:
-            'Enhance your service offerings and streamline processes with our intuitive AI SaaS template.',
-        url: process.env.NEXT_PUBLIC_APP_URL + '/not-found',
-        type: 'website',
-    },
-    alternates: {
-        canonical: `${process.env.NEXT_PUBLIC_APP_URL}/not-found`,
-    },
+      "We couldn't find the page you're looking for. It might have been moved, renamed, or is temporarily unavailable. Let's help you get back on track!",
+    url: process.env.NEXT_PUBLIC_APP_URL + '/not-found',
+    type: 'website',
+  },
+  alternates: {
+    canonical: `${process.env.NEXT_PUBLIC_APP_URL}/not-found`,
+  },
 }
 
 export default function NotFound() {
-    return (
-        <>
-            <div className="relative flex min-h-screen bg-[url(/images/404.jpg)] bg-cover bg-center bg-no-repeat">
-                <span className="absolute inset-0 bg-linear-to-l from-primary/90 via-primary/80 to-primary/90"></span>
-                <div className="container relative z-10 m-auto py-20 text-center">
-                    <div className="text-center">
-                        <div>
-                            <span className="border-webkit absolute mb-5 bg-clip-text text-[100px]/[150px] font-bold tracking-wider text-transparent lg:text-[250px]/[260px]">
-                                404
-                            </span>
-                            <span className="wave-animation mb-5 bg-linear-to-l from-green-100 to-green-light bg-clip-text text-[100px]/[150px] font-bold tracking-wider text-transparent lg:text-[250px]/[260px]">
-                                404
-                            </span>
-                        </div>
-                        <h1 className="mb-2.5 text-2xl font-semibold sm:text-3xl lg:mb-5 lg:text-[40px]/[50px]">
-                            Oops! Page not found.
-                        </h1>
-                        <div className="text-brown mx-auto mb-[30px] max-w-xl space-y-4 text-gray lg:text-lg/[26px]">
-                            <p>
-                                We couldn&apos;t find the page you&apos;re
-                                looking for. It might have been moved, renamed,
-                                or is temporarily unavailable. Let&apos;s help
-                                you get back on track!
-                            </p>
-                        </div>
-                        <Link href="/" className={buttonVariants()}>
-                            <Home className="size-4 shrink-0" />
-                            Back to Home
-                        </Link>
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="relative flex min-h-screen bg-[url(/images/404.jpg)] bg-cover bg-center bg-no-repeat">
+        <span className="from-primary/90 via-primary/80 to-primary/90 absolute inset-0 bg-linear-to-l"></span>
+        <div className="relative z-10 container m-auto py-20 text-center">
+          <div className="text-center">
+            <div>
+              <span className="border-webkit absolute mb-5 bg-clip-text text-[100px]/[150px] font-bold tracking-wider text-transparent lg:text-[250px]/[260px]">
+                404
+              </span>
+              <span className="wave-animation to-green-light mb-5 bg-linear-to-l from-green-100 bg-clip-text text-[100px]/[150px] font-bold tracking-wider text-transparent lg:text-[250px]/[260px]">
+                404
+              </span>
             </div>
+            <h1 className="mb-2.5 text-2xl font-semibold sm:text-3xl lg:mb-5 lg:text-[40px]/[50px]">
+              Oops! Page not found.
+            </h1>
+            <div className="text-brown text-gray mx-auto mb-[30px] max-w-xl space-y-4 lg:text-lg/[26px]">
+              <p>
+                We couldn&apos;t find the page you&apos;re looking for. It might
+                have been moved, renamed, or is temporarily unavailable.
+                Let&apos;s help you get back on track!
+              </p>
+            </div>
+            <Link href="/" className={buttonVariants()}>
+              <Home className="size-4 shrink-0" />
+              Back to Home
+            </Link>
+          </div>
+        </div>
+      </div>
 
-            <script
-                type="application/ld+json"
-                dangerouslySetInnerHTML={{
-                    __html: `{
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: `{
                 "@context": "https://schema.org",
                 "@type": "WebSite",
                 "name": "404",
@@ -81,8 +80,8 @@ export default function NotFound() {
                     }]
                 }
             }`,
-                }}
-            />
-        </>
-    )
+        }}
+      />
+    </>
+  )
 }
