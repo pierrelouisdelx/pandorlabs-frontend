79ab322305c0f026577ddffd01ecff6a31ddf60c
Pierre-Louis <pierrelouisdelcroix2001@gmail.com>
2025-11-04 23:35:08 +0100

    Refactor header and navigation components for improved layout and accessibility. Updated styles in globals.css for consistency and enhanced animations. Adjusted button text and structure in header for better user experience.

--- components/layouts/header.tsx
+++ components/layouts/header.tsx
@@ -4,11 +4,11 @@ import { MenuIcon } from 'lucide-react'
 import NavLinks from '@/components/layouts/nav-links'
 import { buttonVariants } from '@/components/ui/button'
 import {
-    Sheet,
-    SheetContent,
-    SheetDescription,
-    SheetTitle,
-    SheetTrigger,
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
 } from '@/components/ui/sheet'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
@@ -16,58 +16,41 @@ import { useEffect, useState } from 'react'
 import Logo from './logo'
 
 export default function Header() {
-    const [isOpen, setIsOpen] = useState(false)
-    const [isScrolled, setIsScrolled] = useState(false)
-    useEffect(() => {
-        const handleScroll = () => {
-            if (window.scrollY > 50) {
-                setIsScrolled(true)
-            } else {
-                setIsScrolled(false)
-            }
-        }
-        window.addEventListener('scroll', handleScroll)
-        return () => {
-            window.removeEventListener('scroll', handleScroll)
-        }
-    }, [])
+  const [isOpen, setIsOpen] = useState(false)
 
-    const pathName = usePathname()
-    useEffect(() => {
-        setIsOpen(false)
-    }, [pathName])
-    return (
-        <header
-            className={`header sticky top-0 z-50 bg-white/10 backdrop-blur-lg transition ${isScrolled ? 'shadow-[0_0_10px_0_rgba(255,255,255,0.1)]' : ''}`}
-        >
-            <div className="container flex items-center justify-center gap-5 py-3.5 lg:gap-10 lg:py-0">
-                <div className="flex items-center gap-12">
-                    <Logo />
-                    <NavLinks className="hidden lg:flex" />
-                </div>
-                <div className="flex gap-3.5">
-                    <Link
-                        href="/contact-us"
-                        className={`${buttonVariants()} rounded-full`}
-                    >
-                        Contact Us
-                    </Link>
+  const pathName = usePathname()
+  useEffect(() => {
+    setIsOpen(false)
+  }, [pathName])
 
-                    <Sheet open={isOpen} onOpenChange={setIsOpen}>
-                        <SheetTrigger asChild>
-                            <button type="button" className="lg:hidden">
-                                <span className="sr-only">Menu</span>
-                                <MenuIcon className="size-8 text-white" />
-                            </button>
-                        </SheetTrigger>
-                        <SheetContent className="p-0">
-                            <SheetTitle hidden />
-                            <SheetDescription hidden />
-                            <NavLinks />
-                        </SheetContent>
-                    </Sheet>
-                </div>
-            </div>
-        </header>
-    )
+  return (
+    <header className="sticky top-5 z-50 container flex items-center justify-center">
+      <div className="flex w-full items-center justify-between gap-5 rounded-full bg-white/10 px-10 py-2 shadow-[0_0_10px_0_rgba(255,255,255,0.1)] backdrop-blur-lg">
+        <Logo />
+        <NavLinks className="hidden lg:flex" />
+        <div className="flex gap-3.5">
+          <Link
+            href="/contact-us"
+            className={`${buttonVariants()} rounded-full`}
+          >
+            Book a demo
+          </Link>
+
+          <Sheet open={isOpen} onOpenChange={setIsOpen}>
+            <SheetTrigger asChild>
+              <button type="button" className="lg:hidden">
+                <span className="sr-only">Menu</span>
+                <MenuIcon className="size-8 text-white" />
+              </button>
+            </SheetTrigger>
+            <SheetContent className="p-0">
+              <SheetTitle hidden />
+              <SheetDescription hidden />
+              <NavLinks />
+            </SheetContent>
+          </Sheet>
+        </div>
+      </div>
+    </header>
+  )
 }
