f4296a8b682205987a8e64404b3684cba2840b8b
Pierre-Louis <pierrelouisdelcroix2001@gmail.com>
2025-11-05 15:49:02 +0100

    Refactor Header component for improved layout and responsiveness
    - Adjusted grid layout in the Header component to support responsive design, enhancing the display on larger screens.
    - Updated visibility classes for NavLinks and the demo booking button to improve user experience across different screen sizes.

--- components/layouts/header.tsx
+++ components/layouts/header.tsx
@@ -25,15 +25,15 @@ export default function Header() {
 
   return (
     <header className="sticky top-5 z-50 container flex items-center justify-center">
-      <div className="grid w-full grid-cols-[auto_1fr_auto] items-center gap-5 rounded-full bg-white/10 px-5 py-2 shadow-[0_0_10px_0_rgba(255,255,255,0.1)] backdrop-blur-lg">
+      <div className="grid w-full grid-cols-2 lg:grid-cols-[auto_1fr_auto] items-center gap-5 rounded-full bg-white/10 px-5 py-2 shadow-[0_0_10px_0_rgba(255,255,255,0.1)] backdrop-blur-lg">
         <Logo />
-        <div className="flex justify-center">
-          <NavLinks className="hidden lg:flex" />
+        <div className="hidden lg:flex justify-center">
+          <NavLinks className="flex" />
         </div>
-        <div className="flex gap-3.5">
+        <div className="flex justify-end gap-3.5">
           <Link
             href="/contact-us"
-            className={`${buttonVariants()} hidden rounded-full lg:inline-flex`}
+            className={`${buttonVariants()} rounded-full max-lg:!hidden`}
           >
             Book a demo
           </Link>
